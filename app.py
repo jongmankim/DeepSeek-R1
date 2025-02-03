d50be2804a7ecd7e5498292cf1dbcfd446df00cd
jongmankim <kjm270@gmail.com>
2025-02-03 12:58:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ completion = client.chat.completions.create(
     model="lmstudio-community/gemma-2-9b-it-GGUF", 
     messages=[
         {"role": "system", "content": "You are a helpful assistant."}, 
-        {"role": "user", "content": "너는 멀  할 수 있어?"}
+        {"role": "user", "content": "우리는 무엇을 위해 살아가나요?"}
     ],
     temperature=0.7,
 )
@@ -18,5 +18,3 @@ print(completion.choices [0].message)
 #[출처] [python]pip3 install 커맨드 실행 안 되는 오류 해결 errer: externally-managed-environment (mac)|작성자 엎질
 #다음과 같은 명ㅕ령어를 실행하면 해결된다.
 #python3 -m pip config set global.break-system-packages true
-#
-#
